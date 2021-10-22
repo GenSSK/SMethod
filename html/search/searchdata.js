766ea2bc28ec36f0545eaf3922aa86d8c9eb2c9a
GenSSK <g.sasaki@crl.epi.dendai.ac.jp>
2021-10-22 20:12:17 +0900

    update

--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -2,20 +2,23 @@ var indexSectionsWithContent =
 {
   0: "gs~",
   1: "s",
-  2: "gs~"
+  2: "s",
+  3: "gs~"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "functions"
+  2: "files",
+  3: "functions"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Functions"
+  2: "Files",
+  3: "Functions"
 };
 
